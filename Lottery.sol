5397a0d0eb13ffc09479fc98b0c266a2f3e0e02c
HCLiu518 <38768071+HCLiu518@users.noreply.github.com>
2018-04-27 21:46:21 +0800

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -5,14 +5,16 @@ pragma solidity ^0.4.21;
 
 
 contract caller {
+    
     address _addr;
+    event _somethingWrong (string _str2);
     
     function _set (address addr) public{
         _addr = addr;
     }
     
-    function _callLottery (uint32 _gas,bool _theans) public { 
-        _addr.call.value(msg.value).gas(_gas)(bytes4(sha3("play(bool)")),_theans);
+    function _callLottery (bool _theAns) public { 
+        _addr.call.value(msg.value)(bytes4(sha3("play(bool)")),_theAns);
     }
     
     
@@ -21,14 +23,22 @@ contract caller {
 
 contract Lottery is caller {
     
-    function gogo(uint32 _gasuse) public payable  {
+    event weDidIt(string _str, uint _amount);
+    
+    function () payable public {
+        
+    }
+    
+    function gogo() public payable  {
         bytes32 entropy = blockhash(block.number-1);
         bytes1 coinFlip = entropy[0] & 1;
         if (coinFlip == 1) {
-            _callLottery(_gasuse, true);
+            _callLottery(true);
+            emit weDidIt("heads_winner", msg.value);
         }
         else{
-            _callLottery(_gasuse, false);
+            _callLottery(false);
+            emit weDidIt("tails_winner", msg.value);
         }
     }
     
